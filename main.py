aa2c82cfb7707daebbeeee6d69ecfc4dc96c1834
bbhav <bhavna.bhaisare@gmail.com>
2025-11-23 20:08:55 +0530

    env setup

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course-original!")
+    print(os.environ.get("OPEN_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
